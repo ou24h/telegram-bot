e36898ae8f38206ce6be3ee2575ccf510c8b88cb
o.u24h <Np7s.shop@outlook.de>
2025-09-05 05:10:48 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# تحميل ffmpeg
+# ✅ تحميل ffmpeg
 mkdir -p ffmpeg
 curl -L https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip -o ffmpeg.zip
 unzip ffmpeg.zip -d ffmpeg
-mv ffmpeg/ffmpeg-*/* ffmpeg/
-rm -rf ffmpeg.zip
+mv ffmpeg/ffmpeg-*/* ffmpeg
+rm -rf ffmpeg/ffmpeg-*
+rm ffmpeg.zip
 
-# تشغيل البوت
+# ✅ تحميل yt-dlp
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
+chmod +x yt-dlp
+
+# ✅ تشغيل البوت
 node bot.js
