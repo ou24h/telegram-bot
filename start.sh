881eeb4901fae073c207df701549b8fcca9fd8f8
o.u24h <Np7s.shop@outlook.de>
2025-09-05 05:35:12 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ chmod +x yt-dlp
 
 # ✅ تحميل ffmpeg (نسخة static بصيغة tar.xz)
 mkdir -p ffmpeg
-curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz -o ffmpeg.tar.xz
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
 tar -xf ffmpeg.tar.xz --strip-components=1 -C ffmpeg
 rm ffmpeg.tar.xz
 
